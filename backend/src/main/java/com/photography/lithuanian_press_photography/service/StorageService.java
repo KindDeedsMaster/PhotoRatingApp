91d49929cd356cdf68287cdb2a5b7ca6ec347d63
Anton Isakov <anton.isakov23@gmail.com>
2024-09-07 22:38:05 +0300

    created: StorageService and FileUploadController

---
+++ backend/src/main/java/com/photography/lithuanian_press_photography/service/StorageService.java
@@ -0,0 +1,16 @@
+package com.photography.lithuanian_press_photography.service;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.stream.Stream;
+import java.nio.file.Path;
+
+public interface StorageService {
+    void init();
+    void store(MultipartFile file);
+    Stream<Path> loadAll();
+    Path load (String filename);
+    Resource loadAsResource(String filename);
+    void deleteAll();
+}
